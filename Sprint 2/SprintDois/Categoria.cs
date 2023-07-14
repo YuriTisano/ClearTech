2a1f2c317262a1c6ee2ae5e85b6b2e44d5289a55
YuriTisano <139516878+YuriTisano@users.noreply.github.com>
2023-07-13 22:02:17 -0300

    Add files via upload

---
+++ Sprint 2/SprintDois/Categoria.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SprintDois
+{
+    public class Categoria
+    {
+        public string Status = "";
+        public string NomeCategoria { get; set; }
+        public DateTime Horas { get; set; }
+        public bool CampoNulo (string name)
+        {
+            if (String.IsNullOrWhiteSpace(name) || String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Por favor, informe uma categoria com letras e com menos de 128 caracteres");
+            }
+            else
+            {
+                int alpha = Regex.Matches(name, @"[a-zA-Zà-úÀ-Ú' ']").Count;
+                if (name.Length <= 128 && alpha == name.Length)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public void CadastrodeCategoria()
+        {
+            while (true)
+            {
+                Horas = DateTime.Now;
+                Status = "Ativo";
+
+                try
+                {
+                    string cadastroCategoria = Console.ReadLine();
+                    if (CampoNulo(cadastroCategoria))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\nCATEGORIA CADASTRADA!");
+                        Console.WriteLine("Categoria criada: " + cadastroCategoria);
+                        Console.WriteLine("Criado em: " + Horas);
+                        Console.WriteLine("Status: " + Status);
+                        Console.ResetColor();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Por favor, informe uma categoria somente com letras e com menos de 128 caracteres\"");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void EdicaodeCategoria()
+        {
+            while (true)
+            {
+                Horas = DateTime.Now;
+                Status = Status;
+
+                try
+                {
+                    string edicaoCategoria = Console.ReadLine();
+                    NomeCategoria = edicaoCategoria;
+                    if (CampoNulo(edicaoCategoria))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\nCATEGORIA EDITADA!");
+                        Console.WriteLine("Novo nome da categoria: " + edicaoCategoria);
+                        Console.WriteLine("Modificado em: " + Horas);
+                        Console.WriteLine("Status: " + Status);
+                        Console.ResetColor();
+                        break;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor= ConsoleColor.Red;
+                        Console.WriteLine("\nPor favor, informe uma categoria somente com letras e com menos de 128 caracteres\"");
+                        Console.ResetColor();
+                    }
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public void AlteracaodeStatus()
+        {
+            if (Status == "Ativo")
+            {
+                Status = "Inativo";
+            }
+            else
+            {
+
+                Status = "Ativo";
+            }
+        }
+    }
+
+}
---
+++ Sprint 2/SprintDois/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SprintDois
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Categoria categoria = new Categoria();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Bem Vindo ao cadastro de categorias!");
+            Console.ResetColor();
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("\nInforme o que quer cadastrar?");
+                Console.ResetColor();
+
+                categoria.CadastrodeCategoria();
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Gostaria de editar a categoria cadastrada? Digite SIM ou NAO.");
+                    Console.ResetColor();
+
+                    string edit = Console.ReadLine();
+                    if (edit.ToUpper() == "SIM")
+                    {
+                        do
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("\nPor favor, informe um novo nome para a categoria.");
+                            Console.ResetColor();
+
+                            categoria.EdicaodeCategoria();
+
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\nA categoria " + categoria.NomeCategoria + " está " + categoria.Status);
+                            Console.WriteLine("Caso queira alterar o status (ativo e inativo) digite SIM ou NAO.\n");
+                            Console.ResetColor();
+
+                            string usuarioalteraStatus = Console.ReadLine();
+                            if (usuarioalteraStatus.ToUpper() == "SIM")
+                            {
+                                categoria.AlteracaodeStatus();
+                                Console.ForegroundColor = ConsoleColor.Green; //
+                                Console.WriteLine("Categoria criada: " + categoria.NomeCategoria);
+                                Console.ResetColor(); //
+                                Console.ForegroundColor = ConsoleColor.DarkBlue; //
+                                Console.WriteLine("Status: " + categoria.Status);
+                                Console.ResetColor(); //
+                                Console.ForegroundColor = ConsoleColor.Green; //
+                                Console.WriteLine("Edição realizada em: " + DateTime.Now + "\n");
+                                Console.ResetColor();//
+                                break;
+                            }
+                            else if (usuarioalteraStatus.ToUpper() == "NAO")
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine("Tudo bem. Status mantido!");
+                                Console.ResetColor();
+                                break;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nOPS! Não cosegui entender o que você digitou");
+                                Console.ResetColor();
+                            }
+                        }
+                        while (true);
+                    }
+                    else if (edit.ToUpper() == "NAO")
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("\nTudo bem. Categoria mantida.");
+                        Console.ResetColor();
+                        return;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nOPS! Não cosegui entender o que você digitou");
+                        Console.ResetColor();
+                    }
+                }while (true);
+            }while (true);
+        }
+    }
+}
